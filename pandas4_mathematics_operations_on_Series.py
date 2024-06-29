6b227fc444f860c00813d507d61507acc859e5ad
shashank <shashankshekhar8534@gmail.com>
2024-06-29 12:00:00 +0530

    files

---
+++ pandas4_mathematics_operations_on_Series.py
@@ -0,0 +1,8 @@
+import pandas as pd
+s=pd.Series([1,2,3,4,5])
+print(s)
+print("after +2 \n",s+2)
+print("after -2\n",s-2)
+print("after *2\n",s*2)
+print("after /2\n",s/2)
+print("after **2\n",s**2)
